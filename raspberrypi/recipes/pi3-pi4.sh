9ede6bb93fab85a47220ccd6d884310a1eeaff64
Cyril Brulebois <cyril@debamax.com>
2024-07-20 07:52:53 +0200

    Turn the original hook into a toaster recipe.

--- raspberrypi/recipes/pi3-pi4.sh
+++ raspberrypi/recipes/pi3-pi4.sh
@@ -1,25 +1,30 @@
-echo "nameserver 1.1.1.1" > /etc/resolv.conf
-# Install minimal tools
-apt-get update
-apt-get install wget sudo avahi-daemon -y
-apt-get clean
-# Ensure we have the pi user
-useradd -s /bin/bash -Gsudo -m pi
-usermod -aG plugdev pi
-echo "pi:raspberry" | chpasswd
-# Disable root login on SSH
-mkdir -p /etc/ssh/sshd_config.d/
-echo "PermitEmptyPasswords no" > /etc/ssh/sshd_config.d/pirogue-ssh.conf
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config.d/pirogue-ssh.conf
-# Force egenerate SSH host keys if exist and enable SSH
-rm -f /etc/ssh/ssh_host_*
-systemctl enable ssh
-# Change the hostname
-echo "127.0.0.1 pirogue" >> /etc/hosts
-echo "127.0.0.1 pirogue.local pirogue" >> /etc/hosts
-echo "::1 pirogue" >> /etc/hosts
-echo "::1 pirogue.local pirogue" >> /etc/hosts
-echo "pirogue" > /etc/hostname
-# Add PTS PPA
-wget -O /etc/apt/sources.list.d/pirogue.list https://pts-project.org/debian-12/pirogue.list
-wget -O /etc/apt/trusted.gpg.d/pirogue.asc   https://pts-project.org/debian-12/Key.gpg
+# This recipe is sourced by the toaster, don't try to run it!
+
+# shellcheck disable=SC2086
+toast_me() {
+  echo "nameserver 1.1.1.1" > $MNT/etc/resolv.conf
+  # Install minimal tools
+  chroot $MNT apt-get update
+  chroot $MNT apt-get install -y wget sudo avahi-daemon
+  chroot $MNT apt-get clean
+  # Ensure we have the pi user
+  chroot $MNT useradd -s /bin/bash -Gsudo -m pi
+  chroot $MNT usermod -aG plugdev pi
+  echo "pi:raspberry" | chroot $MNT chpasswd
+  # Disable root login on SSH
+  mkdir -p $MNT/etc/ssh/sshd_config.d/
+  echo "PermitEmptyPasswords no" > $MNT/etc/ssh/sshd_config.d/pirogue-ssh.conf
+  echo "PermitRootLogin no" >> $MNT/etc/ssh/sshd_config.d/pirogue-ssh.conf
+  # Force generate SSH host keys if they exist and enable SSH
+  rm -f $MNT/etc/ssh/ssh_host_*
+  chroot $MNT systemctl enable ssh
+  # Change the hostname
+  echo "127.0.0.1 pirogue"               >> $MNT/etc/hosts
+  echo "127.0.0.1 pirogue.local pirogue" >> $MNT/etc/hosts
+  echo "::1 pirogue"                     >> $MNT/etc/hosts
+  echo "::1 pirogue.local pirogue"       >> $MNT/etc/hosts
+  echo "pirogue" > $MNT/etc/hostname
+  # Add PTS PPA
+  chroot $MNT wget -O /etc/apt/sources.list.d/pirogue.list https://pts-project.org/debian-12/pirogue.list
+  chroot $MNT wget -O /etc/apt/trusted.gpg.d/pirogue.asc   https://pts-project.org/debian-12/Key.gpg
+}
